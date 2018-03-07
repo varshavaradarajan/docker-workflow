dbc37d6c82fb7aebdc494e2d122bd2f9037f16fe
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2018-03-07 09:49:18 +0530

    Update script to include env var check.

--- create_pod.sh
+++ create_pod.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+if [ -z $KUBE_TOKEN ]; then
+  echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
+  exit ${2:-1}
+fi
+
 namespace=$(</var/run/secrets/kubernetes.io/serviceaccount/namespace)
 curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" -H "Content-Type: application/json" \
       https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$namespace/pods \
